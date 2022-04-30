b236d44a43bf655527332a2ac213b832863e4148
Tung Le <tunglungtung87@gmail.com>
2022-04-30 16:59:34 +0700

    using redux and rendering data, fetching data in cliend side. preparing to do sth in server side

--- src/client/client.js
+++ src/client/client.js
@@ -1,12 +1,25 @@
 /// starup point for the client side application
+import 'babel-polyfill'; /// Fix regenerator Runtime
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 
+/// Redux
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+/// Reducers
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+
+import createStore from './helpers/createStore';
 
 // /// ES2015 modules work nicely with CommonJS
 // const express = require('express');
@@ -13,8 +16,11 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
+  const store = createStore();
+
   /// Render as string and send to some HTML
-  res.send(renderer(req)); // send back to user
+  /// Some logic to initialize and load data in store
+  res.send(renderer(req, store)); // send back to user
 });
 
 app.listen(3000, () => {
